b64123ebb85f4e309d079c117a4f8bec2b27a3b8
ClarkKe <clark@meta-energy.com.tw>
2026-02-23 10:01:57 +0800

    feat: Update footer links, reorder footer elements, and add lucide-preact dependency.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Twitch: "https://twitch.tv/clark7615",
+      GitHub: "https://github.com/clark7615",
     },
   }),
   afterBody: [],
